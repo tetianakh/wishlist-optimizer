93e7fde8783f4798b2f8b34b41ae616d8d11c702
Tetiana Khotiaintseva <tetiana.khot@gmail.com>
2018-11-12 20:26:57 +0100

    Store token in localstorage

--- src/store/store.js
+++ src/store/store.js
@@ -5,14 +5,21 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    token: null
+    token: localStorage.getItem('user-token')
+  },
+  getters: {
+    isAuthenticated: state => {
+      return state.token !== null
+    }
   },
   mutations: {
     logIn (state, {token}) {
       state.token = token
+      localStorage.setItem('user-token', token)
     },
     logOut (state) {
       state.token = null
+      localStorage.removeItem('user-token')
     }
   }
 })

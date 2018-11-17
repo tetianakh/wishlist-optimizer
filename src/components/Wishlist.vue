be55cba12b5b3f7bf23677cfd06c994ea8cbc9bd
Tetiana Khotiaintseva <tetiana.khot@gmail.com>
2018-11-17 21:44:31 +0100

    Extract language loader into a separate mixin

--- src/components/Wishlist.vue
+++ src/components/Wishlist.vue
@@ -11,42 +11,36 @@
         class="margin">Delete wishlist</b-button>
     </div>
 
-    <new-card modalId="newCardModal" :availableLanguages="availableLanguages"></new-card>
+    <new-card modalId="newCardModal"></new-card>
     <cards-table v-if="hasCards" :cards="wishlist.cards"></cards-table>
   </page>
 </template>
 
 <script>
 import WishlistClient from '../clients/WishlistClient'
-import LanguagesClient from '../clients/LanguagesClient'
 import NewCardButton from './NewCardButton'
 import CardsTable from './CardsTable'
 import Pricing from './Pricing'
 import Page from './Page'
 import NewCard from './NewCard'
 import hasCards from '../mixins/hasCards'
-import {NEW_CARD, UPDATE_CARD, DELETE_CARD, CALCULATE_PRICING} from '../events'
+import languagesLoader from '../mixins/languagesLoader'
+import {NEW_CARD, UPDATE_CARD, DELETE_CARD} from '../events'
 
 export default {
   components: {Pricing, Page, NewCard, NewCardButton, CardsTable},
   data () {
     return {
       wishlist: {},
-      wishlistClient: new WishlistClient(),
-      languagesClient: new LanguagesClient(),
-      searchedCards: [],
-      availableLanguages: []
+      wishlistClient: new WishlistClient()
     }
   },
   mounted () {
-    this.languagesClient.getAvailableLanguages().then(languages => {
-      this.$store.commit('setLanguages', languages)
-    })
     this.$eventBus.$on(NEW_CARD, this.addCard)
     this.$eventBus.$on(UPDATE_CARD, this.updateCard)
     this.$eventBus.$on(DELETE_CARD, this.deleteCard)
   },
-  mixins: [hasCards],
+  mixins: [hasCards, languagesLoader],
   created () {
     this.wishlistClient.getWishlist(this.$route.params.id).then(resp => {
       this.wishlist = resp.wishlist
@@ -72,9 +66,6 @@ export default {
       this.wishlistClient.updateCard(this.$route.params.id, card).then(resp => {
         this.wishlist.cards[idx] = resp.card
       })
-    },
-    submitPricingJob () {
-      this.$eventBus.$emit(CALCULATE_PRICING, this.wishlist)
     }
   }
 }
--- src/events.js
+++ src/events.js
@@ -1,6 +1,5 @@
 const NEW_CARD = 'NewCard'
 const UPDATE_CARD = 'UpdateCard'
 const DELETE_CARD = 'DeleteCard'
-const CALCULATE_PRICING = 'CalculatePricing'
 
-export {NEW_CARD, UPDATE_CARD, DELETE_CARD, CALCULATE_PRICING}
+export {NEW_CARD, UPDATE_CARD, DELETE_CARD}
